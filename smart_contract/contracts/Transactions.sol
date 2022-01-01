fbb1507793c3b73775b80938a817c0ad67c8f523
manqn <hamorimark@hotmail.com>
2022-01-01 19:10:42 +0100

    Solidity comments

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,10 +2,28 @@
 
 pragma solidity ^0.8.0;
 
+
+// Contract has similarities to 'class" in other languages (class variables, inheritance, etc.)
 contract Transactions {
+    // u = unsigned - represent positive integers, not positive and negative integers
+    // int = integer
+    // 256 - 256 bits in size
     uint256 transactionCount;
 
+    // B. Events
+    // Events are notify external parties; easy to search and
+    // access events from outside blockchain (with lightweight clients)
+    // typically declare after contract parameters
+
+    // Typically, capitalized - and add Log in front to be explicit and prevent confusion
+    // with a function call
+
+    // Declare
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    // Unlike other languages, CANNOT iterate through all elements in
+    // mapping, without knowing source keys - can build data structure
+    // on top to do this
   
     struct TransferStruct {
         address sender;
